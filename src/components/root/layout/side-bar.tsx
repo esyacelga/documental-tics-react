59fff40071e60a5a405a308f5e79c37940d210d9
esyacelga <esyacelga@hotmail.com>
2023-11-26 22:10:48 -0500

    Material UI configuration on vite project

--- src/components/root/layout/side-bar.tsx
+++ src/components/root/layout/side-bar.tsx
@@ -13,24 +13,17 @@ import {
 } from '@mui/material'
 import {TurnedInNot} from '@mui/icons-material';
 import {useTranslation} from 'react-i18next';
-import i18n from "../../../infrastructure/i18n.ts";
 
 
 export const SideBar = ({drawerWidth = 240}) => {
     const {t} = useTranslation();
     //i18n.changeLanguage('es');
-    const changeLanguageToSpanish = () => {
-        i18n.changeLanguage('es');
-    };
+
     return (
         <Box
             component='nav'
             sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
         >
-            <div>
-                <button onClick={changeLanguageToSpanish}>....{t('Switch to Spanish')}</button>
-                {/* Agrega más botones o lógica para cambiar a otros idiomas si lo necesitas */}
-            </div>
             <Drawer
                 variant='permanent' // temporary
                 open
@@ -44,7 +37,6 @@ export const SideBar = ({drawerWidth = 240}) => {
                         Fernando Herrera...
                         <h1>{t('hello')}</h1>
                     </Typography>
-                    <button onClick={changeLanguageToSpanish}>Hola</button>
                 </Toolbar>
                 <Divider/>
 
--- src/infrastructure/i18n.ts
+++ src/infrastructure/i18n.ts
@@ -1,19 +1,32 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 
-i18n.use(initReactI18next).init({
-    resources: {
-        "es": {
-            "hello": "¡Hola!ssd",
-            "welcome": "Bienvenido a mi aplicación.asas"
+i18n
+    .use(LanguageDetector) // Agregar el complemento LanguageDetector
+    .use(initReactI18next)
+    .init({
+        // Configuración de traducción
+        resources: {
+            en: {
+                translation:{
+                    "hello": "¡Hola!",
+                    "welcome": "Bienvenido a mi aplicación."
+                }
+            },
+            es: {
+                translation: {
+                    "hello": "¡Hola!",
+                    "welcome": "Bienvenido a mi aplicación."
+                }
+            }
+        },
+        fallbackLng: 'es', // Idioma predeterminado si no se encuentra una traducción
+        debug: true, // Habilitar mensajes de depuración en la consola
+        interpolation: {
+            escapeValue: false // No escapar valores HTML en las traducciones
         }
-    },
-    lng: 'es', // idioma por defecto
-    interpolation: {
-        escapeValue: false, // no necesitas escapar los valores
-    },
-
-});
+    });
 
 export default i18n;
